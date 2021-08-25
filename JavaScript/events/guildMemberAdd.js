719c3b3aa310e5ac645d1fbc9813e65660dd53aa
Enoughsdv <enoughsdv@gmail.com>
2021-08-25 16:14:10 -0500

    again

--- JavaScript/events/guildMemberAdd.js
+++ JavaScript/events/guildMemberAdd.js
@@ -4,51 +4,22 @@ const Canvas = require("canvas");
 module.exports = async (client, member) => {
   const guild = member.guild;
   guild.channels.cache
-<<<<<<< HEAD
-    .get("CHANNEL ID")
-=======
     .get(client.config.countPlayers)
->>>>>>> Fixing bugs v3
     .setName("🎮 | Users: " + guild.memberCount);
 
   ///////////// WELCOME WITH CANVAS
   const canvas = Canvas.createCanvas(800, 360);
   Canvas.registerFont("./Yusei.ttf", { family: "Yusei" });
   const ctx = canvas.getContext("2d");
-<<<<<<< HEAD
-
-  const background = await Canvas.loadImage(
-    "LINK"
-  );
-=======
   
   const background = await Canvas.loadImage(client.config.backgroundWelcomeLink);
 
->>>>>>> Fixing bugs v3
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
   ctx.font = "35px Yusei";
   ctx.fillStyle = "#fff";
   ctx.textAlign = "center";
   ctx.textBaselin = "hanging";
-<<<<<<< HEAD
-  ctx.fillText(
-    `Welcome ${member.user.username} to Eazv.`,
-    canvas.width / 2,
-    295
-  );
-  ctx.fillText(
-    `We are currently ${guild.memberCount} members`,
-    canvas.width / 2,
-    337
-  );
-  const y = 70,
-    radio = 85,
-    x = canvas.width / 2 - radio;
-  ctx.beginPath();
-  ctx.arc(x + radio, y + radio, radio + 4, 0, Math.PI * 2, true);
-  ctx.fillStyle = "#fff";
-=======
 
   //1 line
   ctx.fillText(`Welcome ${member.user.username} to Eazv.`, canvas.width / 2, 295);
@@ -59,7 +30,6 @@ module.exports = async (client, member) => {
   ctx.beginPath();
   ctx.arc(x + radio, y + radio, radio + 4, 0, Math.PI * 2, true);
   ctx.fillStyle = client.config.circleImageWelcome;
->>>>>>> Fixing bugs v3
   ctx.fill();
   ctx.stroke();
   ctx.closePath();
@@ -71,34 +41,16 @@ module.exports = async (client, member) => {
 
   ctx.drawImage(
     await Canvas.loadImage(
-<<<<<<< HEAD
-      member.user.displayAvatarURL({ dynamic: true, size: 256, format: "png" })
-    ),
-    x,
-    y,
-    radio * 2,
-    radio * 2
-  );
-  const attach = new Discord.MessageAttachment(
-    canvas.toBuffer(),
-    "eazv-dev.gif"
-  );
-=======
       member.user.displayAvatarURL({ dynamic: true, size: 256, format: "png" })),
       x, y, radio * 2, radio * 2);
 
   const attach = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.gif");
->>>>>>> Fixing bugs v3
 
   /////////////////////////////////////////////////////////
 
   /////////////////// LOG
   function formatDate(template, date) {
-<<<<<<< HEAD
-    let specs = "YYYY:MM:DD:HH:mm:ss".split(":");
-=======
     const specs = "YYYY:MM:DD:HH:mm:ss".split(":");
->>>>>>> Fixing bugs v3
     date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4);
     return date
       .toISOString()
@@ -108,19 +60,10 @@ module.exports = async (client, member) => {
       }, template);
   }
 
-<<<<<<< HEAD
-  let logEmbed = new Discord.MessageEmbed()
-    .setAuthor(`${member.user.tag}`)
-    .setDescription(`📗 <@${member.user.id}> **has joined the server**`)
-    .addField(
-      "**Account created:**",
-      formatDate("DD/MM/YYYY, at HH:mm:ss", member.user.createdAt)
-=======
   const logEmbed = new Discord.MessageEmbed()
     .setAuthor(`${member.user.tag}`)
     .setDescription(`📗 <@${member.user.id}> **has joined the server**`)
     .addField("**Account created:**", formatDate("DD/MM/YYYY, at HH:mm:ss", member.user.createdAt)
->>>>>>> Fixing bugs v3
     )
     .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
     .setFooter(`ID: ${member.user.id}`)
@@ -128,11 +71,6 @@ module.exports = async (client, member) => {
     .setColor("GREEN");
 
   client.channels.cache.get(client.config.logChannel).send(logEmbed);
-<<<<<<< HEAD
-  member.roles.add("ID ROLE");
-  member.guild.channels.cache.get("CHANNEL ID WELCOME").send(attach);
-=======
   member.roles.add(client.config.userJoinRole);
   member.guild.channels.cache.get(client.config.welcomeChannel).send(attach);
->>>>>>> Fixing bugs v3
 };
